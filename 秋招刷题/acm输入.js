d2a7585c1eacb498893829ed380cb5e399f6ae46
chenjiayi949 <chenjiayi949@hellobike.com>
2025-08-10 23:01:05 +0800

    feat: nodejs输入输出模版

---
+++ 秋招刷题/acm输入.js
@@ -0,0 +1,19 @@
+const rl = require('readline').createInterface({ input: process.stdin });
+
+const lines = [];
+rl.on('line', line => {
+    lines.push(line);
+});
+const main = () => {
+    // 书写输入的数据逻辑，将输入的数据逻辑传入解决函数     
+    
+    solve() 
+}
+
+const solve = () => {
+    
+}
+
+rl.on('close', () => {    
+    main()     
+}); 
